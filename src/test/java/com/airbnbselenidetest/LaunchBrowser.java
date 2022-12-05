a310f879906a6066a9f8db5c17405f459a153c73
devop <devops.engineering06@gmail.com>
2022-12-05 22:28:27 +0300

    Chrome/Firefox/Safari  Browsers with HeadLess,   Maximized mode |    Configuration

--- src/test/java/com/airbnbselenidetest/LaunchBrowser.java
+++ src/test/java/com/airbnbselenidetest/LaunchBrowser.java
@@ -14,29 +14,33 @@ public class LaunchBrowser {
 
     @Test
     public void launchBrowserTest() {
-
-
 //        ------------------------ 1. yol------------------------
+        //System.setProperty("webdriver.gecko.driver", "/c/Selenium/geckodriver");
         //Configuration.browser = "firefox";
         Configuration.browser = "chrome";
-        Configuration.headless = true;
+//  ------------------------      Tarayıcılar açılmadan test yapılsın-------------------
+        Configuration.headless = false;
 //        ------------------------ 2. yol------------------------
         //System.setProperty("selenide.browser","chrome");
         // mvn clean install -Dselenide.browser="chrome"       --> Jenkins  için yazılacak code
-
 //        veya başka bir tarayıcı için test
-
         //System.setProperty("selenide.browser","firefox");
         // mvn clean install -Dselenide.browser="firefox"       --> Jenkins  için yazılacak code
+//------------------------------------------------------------------------------------------------------------------------
+        Configuration.baseUrl = "https://www.google.com.tr";
+        Configuration.browserSize = "1920x1080";
+        Configuration.screenshots = false;
 
+        //Configuration.browserBinary = "/Users/devop/chromedriver";
+        System.setProperty("selenide.browserBinary", "/c/Selenium/chromedriver");
+        //System.setProperty("selenide.browserBinary", "/c/Selenium/geckodriver");
 
-        open("https://www.google.com.tr");
+        open("/");
         $(By.name("q")).setValue("Naveen AutomationLabs");
         $(By.name("btnK")).click();
         $(By.id("logo")).shouldHave(Condition.appear);
         String header = $(By.xpath("//h3[text()='Welcome to Naveen AutomationLabs - naveen automationlabs']")).getText();
         System.out.println("header = " + header);
         Assert.assertEquals(header, "Welcome to Naveen AutomationLabs - naveen automationlabs");
-
     }
 }
